1be2d97e4e5c03bc2208306f8a1fb480b9de0db0
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-03-12 11:26:16 +0530

    Task 1 - Creation of Employee class

---
+++ 02_12_Mar_2023_Lab_01/EmailApplication/src/com/greatlearning/javafsd/emailapp/Employee.java
@@ -0,0 +1,71 @@
+package com.greatlearning.javafsd.emailapp;
+
+public class Employee {
+
+//	public static int empCount;
+	
+	public String firstName;
+	
+//	private char firstName;
+	
+	private String lastName;
+	private String department;
+	
+	public Employee(String firstName, String lastName, String department) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+	}
+
+//	public Employee(char firstName, String lastName, String department) {
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.department = department;
+//	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+//	public char getFirstName() {
+//		return firstName;
+//	}
+//	
+//	public void setFirstName(char firstName) {
+//		this.firstName = firstName;
+//	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+//	public void m1() {
+//		System.out.println(empCount);
+//	}
+	
+//	public static void incrementCount() {
+//		empCount ++;
+//		System.out.println(empCount);
+//	}
+//
+//	public static void decrementCount() {
+//		empCount --;
+//		System.out.println(empCount);
+//	}
+}
---
+++ 02_12_Mar_2023_Lab_01/EmailApplication/test/com/greatlearning/javafsd/emailapp/test/EmployeeTest.java
@@ -0,0 +1,21 @@
+package com.greatlearning.javafsd.emailapp.test;
+
+import com.greatlearning.javafsd.emailapp.Employee;
+
+public class EmployeeTest {
+
+	
+	public static void main(String[] args) {
+		
+		Employee ashok = 
+			new Employee("Ashok", "Kumar", "Technical");
+		
+//		ashok = new Employee("Ashok", "Kumar", "Technical");
+		ashok.setFirstName("Ashok");
+		
+//		System.out.println(Employee.empCount);
+//		System.out.println(Employee.);
+		
+//		Employee.incrementCount();
+	}
+}
