d72122130d16109d1208c95339f32824e24594f9
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-03-12 11:38:36 +0530

    Task 2 - Interface Creation / Design

--- 02_12_Mar_2023_Lab_01/EmailApplication/src/com/greatlearning/javafsd/emailapp/Employee.java
+++ 02_12_Mar_2023_Lab_01/EmailApplication/src/com/greatlearning/javafsd/emailapp/Employee.java
@@ -11,6 +11,9 @@ public class Employee {
 	private String lastName;
 	private String department;
 	
+	private String emailAddress;
+	private String password;
+	
 	public Employee(String firstName, String lastName, String department) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -54,6 +57,22 @@ public class Employee {
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 //	public void m1() {
 //		System.out.println(empCount);
@@ -68,4 +87,5 @@ public class Employee {
 //		empCount --;
 //		System.out.println(empCount);
 //	}
+
 }
---
+++ 02_12_Mar_2023_Lab_01/EmailApplication/src/com/greatlearning/javafsd/emailapp/ICredentialService.java
@@ -0,0 +1,15 @@
+package com.greatlearning.javafsd.emailapp;
+
+public interface ICredentialService {
+
+	String generatePassword();
+	
+//	generateEmailAddress(String firstName, 
+//			String lastName, String department, 
+//				String param4, String param5);
+//	
+	String generateEmailAddress(Employee employee);
+
+	void displayCredentials(Employee employee);
+	
+}
