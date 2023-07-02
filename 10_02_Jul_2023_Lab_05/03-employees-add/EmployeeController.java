dcec30f8c6b9dfd24241368becade6a02811915d
sr1983-it-gl <100977885+sr1983-it-gl@users.noreply.github.com>
2023-07-02 17:07:18 +0530

    03-Employee-Add

---
+++ 10_02_Jul_2023_Lab_05/03-employees-add/EmployeeController.java
@@ -0,0 +1,20 @@
+	@GetMapping("/employees/new")
+	public String addEmployeeStep1(Model model) {
+		
+		Employee employee = new Employee();
+		// Default values
+		
+		model.addAttribute("employee", employee);
+		
+		return "add_employee";
+	}
+	
+	@PostMapping("/employees")
+	public String addEmployeeStep2(
+		@ModelAttribute("employee")
+		Employee employee) {
+		
+		employeeService.saveEmployee(employee);
+		return "redirect:/employees";
+	}
+
