ee9178318b496870f0e64c55fd1197951b86aab0
sr1983-it-gl <100977885+sr1983-it-gl@users.noreply.github.com>
2023-07-02 15:27:15 +0530

    01-Welcome Page

---
+++ 10_02_Jul_2023_Lab_05/01-welcome-page/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.greatlearning.ems.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String showWelcomePage() {
+		return "welcome";
+	}
+}
